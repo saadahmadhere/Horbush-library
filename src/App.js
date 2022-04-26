3504fc647b0a35b87725435fccea1d7c3d0179b3
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-26 08:11:22 +0530

    [feat] setup routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import { Navbar, Footer } from "./components";
+import {
+  Home,
+  History,
+  Liked,
+  Login,
+  Playlist,
+  Signup,
+  WatchLater,
+  Error,
+} from "./screens";
+
 const App = () => {
-  return <div>Hi from app.</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/liked" element={<Liked />} />
+        <Route path="/playlist" element={<Playlist />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/watch-later" element={<WatchLater />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
