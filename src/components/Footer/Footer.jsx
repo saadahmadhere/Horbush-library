59acfad1a7f8f7076c0615c333758f17ad985620
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-30 10:06:16 +0530

    feat: add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,44 @@
+import { NavLink } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer id="footer" className="flex_col pt_2 ">
-      <h6 className="h6 footer_heading">
-        Made with &#123; &#9829; &#125; by
-        <a href="https://saadahmad.netlify.app/"> Saad Ahmad</a>
-      </h6>
-      <div className="footer_social">
-        <a href="https://twitter.com/imsaadahmad" target="_blank">
-          <i className="fab fa-twitter fa-sm"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/saadahmadhere/" target="_blank">
-          <i className="fab fa-linkedin-in fa-sm"></i>
-        </a>
-        <a href="https://github.com/saadahmadhere" target="_blank">
-          <i className="fab fa-github fa-sm"></i>
-        </a>
-      </div>
-      <h6>v 1.0.0</h6>
+    <footer className="footer">
+      <NavLink
+        to="/"
+        style={({ isActive }) => {
+          return { color: isActive && "#ff0000" };
+        }}>
+        <i className="fas fa-compass mr_1 mb_1"></i>Explore
+      </NavLink>
+      <NavLink
+        to="/playlist"
+        style={({ isActive }) => {
+          return { color: isActive && "#ff0000" };
+        }}>
+        <i className="fas fa-folder-plus mr_1 mb_1"></i>Playlist
+      </NavLink>
+      <NavLink
+        to="/liked"
+        style={({ isActive }) => {
+          return { color: isActive && "#ff0000" };
+        }}>
+        <i className="fas fa-thumbs-up mr_1 mb_1"></i>Liked
+      </NavLink>
+      <NavLink
+        to="/watch-later"
+        style={({ isActive }) => {
+          return { color: isActive && "#ff0000" };
+        }}>
+        <i className="fas fa-bookmark mr_1 mb_1"></i>Watch Later
+      </NavLink>
+      <NavLink
+        to="/history"
+        style={({ isActive }) => {
+          return { color: isActive && "#ff0000" };
+        }}>
+        <i className="fas fa-history mr_1 mb_1"></i>History
+      </NavLink>
     </footer>
   );
 };
