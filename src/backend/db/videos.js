295dbebb9c7bac672450773e9e753842af9aa115
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-05-14 20:45:30 +0530

    [chore]: add vidoes data in the db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,150 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "712a2c3e-abb3-4b7f-8682-de369c5783d8",
+    title: "Joe Rogan Experience - Jordan Peterson",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Jordan Peterson is a clinical psychologist and tenured professor of psychology at the University of Toronto.",
+    creator: "Powerful JRE",
+    thumbnail:
+      "https://i.ytimg.com/vi/6T7pUEZfgdI/hqdefault.jpg?s%E2%80%A6AFwAcABBg==&rs=AOn4CLCIeXV9LofMjA_u9k2qiiKMKas2DA",
+    src: "https://www.youtube.com/watch?v=6T7pUEZfgdI&t=29s",
+    creatorProfile:
+      "https://yt3.ggpht.com/ytc/AKedOLRkQQQepdHNQ30TP5ywVn7paxmZc5MAynPnkzLg6g=s176-c-k-c0x00ffffff-no-rj",
+    views: 22838707,
+    category: "psychology",
+    uploaded: new Date("Jan 31, 2018 12:32:06"),
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "7872a63e-eb6f-4170-aaef-859b645eaeda",
+    title: "Searching For Light with Radhanath Swami | Rich Roll Podcast",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Radhanath Swami is a spiritual guru, mystic, author & humanitarian. A monk in the Bhakti tradition for nearly 50 years, Radhanath has dedicated his life to helping people find meaning, whilst leading social initiatives which feed, house and heal millions of those in need.",
+    creator: "Rich Roll",
+    thumbnail:
+      "https://i.ytimg.com/vi/PEmaWdfUUuk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCpLPurCN8GDc2vp5YJAMZNWf-JAQ",
+    creatorProfile:
+      "https://yt3.ggpht.com/UXI7ZyoQPoHhboBBUD8pscuva2dd-hlTxMjRDHVg96eoHOfiru5CFoWvkVe8pFiyvlB33_ceow=s68-c-k-c0x00ffffff-no-rj",
+    views: 107960,
+    category: "spiritual",
+    uploaded: new Date("Sep 3, 2020  15:05:18"),
+  },
+  {
+    _id: "61bfbea9-72d3-4410-b601-ffd689136154",
+    title:
+      "Turning Fear into Power: Understanding and managing anxiety - Longwood Seminar",
+    description:
+      "Fear is one of the most important survival mechanisms in all species. But for the millions of Americans living with anxiety disorders, this healthy defense system has a dark side. In this seminar, Harvard Medical School scientists will explore the roots and origins of anxiety and how it can be managed.",
+    creator: "Harvard Medical School",
+    thumbnail:
+      "https://i.ytimg.com/vi/KPvd0Xuib3A/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD1mi4Pa4ZUXSemEp1Y2nGPp0j4-A",
+    creatorProfile:
+      "https://yt3.ggpht.com/ytc/AKedOLRTsafKuC4vOTQE3Bih3jenydRdtP0n-hV60ne9sQ=s68-c-k-c0x00ffffff-no-rj",
+    views: 1019207,
+    category: "self help",
+    uploaded: new Date("Apr 20, 2017  12:25:54"),
+  },
+  {
+    _id: "1a2be212-d830-4449-8aef-4aabe744eab9",
+    title:
+      "Change Your Brain: Neuroscientist Dr. Andrew Huberman | Rich Roll Podcast",
+    description:
+      "Dr. Huberman is here to school us on all things neuroplasticity---and how we can use it to our advantage through intense focus, mindfulness, and restorative sleep. Enjoy!",
+    creator: "Rich Roll",
+    thumbnail:
+      "https://i.ytimg.com/vi/SwQhKFMxmDY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDwRcgQTOsDaUSZ9d94un9xpNxNGA",
+    creatorProfile:
+      "https://yt3.ggpht.com/UXI7ZyoQPoHhboBBUD8pscuva2dd-hlTxMjRDHVg96eoHOfiru5CFoWvkVe8pFiyvlB33_ceow=s88-c-k-c0x00ffffff-no-rj",
+    views: 10642779,
+    category: "psychology",
+    uploaded: new Date("Jul 30, 2020 10:12:06"),
+  },
+  {
+    _id: "006ab9cc-fbc1-4d39-9d98-c797f9b64b17",
+    title: "THE ENTREPRENEUR'S MINDSET | Gary Vaynerchuk USC Talk 2019",
+    description:
+      "This was one of Gary’s most valuable fireside chats in a long time. He spoke with David Belasco, Executive Director of the Lloyd Grief Center for Entrepreneurial Studies. Gary talked about putting $183,000/year on a pedestal, the best class to take in college, the biggest poison in our society, and the 21st Century Entrepreneurial Blueprint.",
+    creator: "GaryVee",
+    thumbnail:
+      "https://i.ytimg.com/vi/LVc0Qw1LSm4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCNIP5VnKccWylq6JbNdbo-wkj7hQ",
+    creatorProfile:
+      "https://yt3.ggpht.com/ql0B19qH4Fa027W_O5YaowJqgveGRgwiC3zdJ1Lqcl-ry2_za4qEQIrT8lBaHrC1QWyHtPXM-w=s68-c-k-c0x00ffffff-no-rj",
+    views: 830385,
+    category: "business",
+    uploaded: new Date("Jun 17, 2019 16:26:54"),
+  },
+  {
+    _id: "26fe1969-7c50-4fdd-b892-aeec553dfa26",
+    title:
+      "How To BRAINWASH Yourself For Success & Destroy NEGATIVE THOUGHTS! | Dr. Joe Dispenza",
+    description:
+      "Dr. Joe Dispenza is teaching the world how to empower and heal our mind through meditation and mindfulness. His studies have proven that when well practiced these tools can put us on the path to understanding and breaking deep-rooted bad habits and even heal illnesses. The author of Becoming Supernatural explains how to stop your mind from controlling you on this episode of Impact Theory with Tom Bilyeu.",
+    creator: "Tom Bilyeu",
+    thumbnail:
+      "https://i.ytimg.com/vi/La9oLLoI5Rc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB5mx3I014FJIyk0tbKJjYZffjgfw",
+    creatorProfile:
+      "https://yt3.ggpht.com/ytc/AKedOLSaycm_2XDkV6ZvJ4NzNUL3cDEiQzcTLWrVx0VXHg=s88-c-k-c0x00ffffff-no-rj",
+    views: 10642779,
+    category: "psychology",
+    uploaded: new Date("Jun 12, 2018 01:36:58"),
+  },
+  {
+    _id: "3f4f9462-3295-4ad7-8412-74faf82415f0",
+    title:
+      "How to Develop Spiritual Personality | July 16, 2006 | Maulana Wahiduddin Khan",
+    description: "Words of wisdom",
+    creator: "Centre for Peace and Spirituality International",
+    thumbnail:
+      "https://i.ytimg.com/vi/nxFUXRFboW4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBl4eK4_-_gMOe2gB2vygcmu5f4hQ",
+    creatorProfile:
+      "https://yt3.ggpht.com/uX3aLT75U8b2bCdAPSmppA0h-H4CMcFl_6RufscgwK7HRp7lS4IU2cx6ElrZttYCQowJ-BRPGg=s68-c-k-c0x00ffffff-no-rj",
+    views: 1812,
+    category: "spiritual",
+    uploaded: new Date("Jan 10, 2020 14:42:28"),
+  },
+  {
+    _id: "9e261c89-96e0-4b3c-9e08-26a301965f3a ",
+    title:
+      "DO THIS Every Morning To Destroy LAZINESS & PROCRASTINATION Today! | Mel Robbins",
+    description:
+      "Mel Robbins is a powerhouse speaker with one of the top 20 TEDx talks in the world. This former criminal defense attorney turned on-air commentator and CNN contributor exposes the myth of motivation and explains how to make the micro-decisions that will launch you into success in this episode of Impact Theory with Tom Bilyeu.",
+    creator: "Tom Bilyeu",
+    thumbnail:
+      "https://i.ytimg.com/vi/LCHPSo79rB4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCdbX83I4hbEURM6slCE3J-SyQGdg",
+    creatorProfile:
+      "https://yt3.ggpht.com/ytc/AKedOLSaycm_2XDkV6ZvJ4NzNUL3cDEiQzcTLWrVx0VXHg=s88-c-k-c0x00ffffff-no-rj",
+    views: 3129111,
+    category: "psychology",
+    uploaded: new Date("Jan 31, 2017  13:44:08"),
+  },
+  {
+    _id: "0dc0c1b1-9636-4e56-aef3-9953ba78810a",
+    title: "How Your Brain Can Turn Anxiety into Calmness",
+    description:
+      "Physician, author, speaker, researcher, and consultant Martin L. Rossman, MD, discusses how to use the power of the healing mind to reduce  stress and anxiety, relieve pain, change lifestyle habits, and live with more wellness.",
+    creator: "UCTV",
+    thumbnail:
+      "https://i.ytimg.com/vi/KYJdekjiAog/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB_4LqWo1C4nRlnFOfuMAP6Nk1uiA",
+    creatorProfile:
+      "https://yt3.ggpht.com/ytc/AKedOLS2MLKFK1IuCoOdUdXOQJz2yXFdNJKDe14Ea94wqg=s68-c-k-c0x00ffffff-no-rj",
+    views: 3621790,
+    category: "self help",
+    uploaded: new Date(" Mar 13, 2010 13:44:08"),
+  },
+
+  {
+    _id: "7c861043-a88a-4ad3-876e-495371a864b8",
+    title:
+      "Multi-Millionaire Cuts The BULLSH*T & Reveals How To ACTUALLY GET RIGH | Tilman Fertittia",
+    description:
+      "When times are really bad, we forget they’re ever going to be good again. And when times are really good, we forget they’re ever going to be bad again.",
+    creator: "Tom Bilyeu",
+    thumbnail:
+      "https://i.ytimg.com/vi/TgJvwVtENsg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBJj28UadLu82g5HkM8ksEVF_wWdQ",
+    creatorProfile:
+      "https://yt3.ggpht.com/ytc/AKedOLSaycm_2XDkV6ZvJ4NzNUL3cDEiQzcTLWrVx0VXHg=s88-c-k-c0x00ffffff-no-rj",
+    views: 1344308,
+    category: "business",
+    uploaded: new Date(" Oct 29, 2019 14:10:12"),
   },
 ];
