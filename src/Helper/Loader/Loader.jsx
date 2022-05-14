e40c2239aa5e9532b5b703b5fff25caaf6c661d7
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-05-14 20:46:20 +0530

    [feat]: add videos

---
+++ src/Helper/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import "./Loader.css";
+const Loader = () => {
+  return (
+    <main className="loader">
+      <i className="fas fa-spinner fa-spin fa-4x"></i>
+    </main>
+  );
+};
+
+export { Loader };
