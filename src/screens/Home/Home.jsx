6cb8e4dac352a12460104d6a6a5c2936433dd8d2
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-05-14 20:48:32 +0530

    [feat]: initilal setup to dispaly vidoes

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,21 +1,24 @@
+import { useEffect, useState } from "react";
 import { Chip, Card } from "../../components";
+import { Loader, getVideos } from "../../Helper";
 import "./Home.css";
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    getVideos(setIsLoading, setVideos);
+  }, []);
+
   return (
     <main className="main_home">
       <Chip />
       <div className="home_card">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          videos.map((video) => <Card video={video} key={video._id} />)
+        )}
       </div>
     </main>
   );
