7e4cc7d42172ea3c66c43307a05620d1db2e8c26
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-06-12 12:50:18 +0530

    [update]: import Auth to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
+import { useAuth } from "../../Helper/Auth/auth-context";
+import { logout } from "../../Helper";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const {
+    userState: {
+      token,
+      user: { firstName },
+    },
+    dispatchUserState,
+  } = useAuth();
+
+  const logoutHandler = () => {
+    logout(dispatchUserState, navigate);
+  };
+
   return (
     <>
       <nav className="navbar flex">
@@ -23,9 +38,18 @@ const Navbar = () => {
 
         <ul className="navbar_links flex list_style_none">
           <li>
-            <Link to="/login" className="btn">
-              <i className="fas fa-user-circle fa-2x"></i>
-            </Link>
+            {token ? (
+              <div className="flex">
+                <span className="txt_sm">Hi, {firstName}</span>
+                <button className="btn btn_outline" onClick={logoutHandler}>
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <Link to="/login" className="btn">
+                <i className="fas fa-user-circle fa-2x"></i>
+              </Link>
+            )}
           </li>
         </ul>
       </nav>
