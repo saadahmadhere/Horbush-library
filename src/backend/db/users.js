d2020f6805f75e97b1e1ef6edba186240809b2b7
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-06-12 12:46:49 +0530

    [update]: change default user

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Chandler",
+    lastName: "Bing",
+    email: "chandler@friends.com",
+    password: "chandlerBing123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
