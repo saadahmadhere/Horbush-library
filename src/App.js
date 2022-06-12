60e0184705e8216313c287dcdc90955614753aad
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-06-12 12:45:38 +0530

    [feat]:  import auth to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Navbar, Footer } from "./components";
+import { Toaster } from "react-hot-toast";
+import { RequiresAuth } from "./components/RequiresAuth";
 import {
   Home,
   History,
@@ -15,16 +17,45 @@ import {
 const App = () => {
   return (
     <>
+      <Toaster position="top-center" reverseOrder={false} gutter={8} />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore/:id" element={<SingleVideoPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/liked" element={<Liked />} />
-        <Route path="/playlist" element={<Playlist />} />
+        <Route
+          path="/history"
+          element={
+            <RequiresAuth>
+              <History />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/liked"
+          element={
+            <RequiresAuth>
+              <Liked />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/playlist"
+          element={
+            <RequiresAuth>
+              <Playlist />
+            </RequiresAuth>
+          }
+        />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/watch-later" element={<WatchLater />} />
+        <Route
+          path="/watch-later"
+          element={
+            <RequiresAuth>
+              <WatchLater />
+            </RequiresAuth>
+          }
+        />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
