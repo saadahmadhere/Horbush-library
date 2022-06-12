f0873f73bd8ed4ef94bbc438f06348c17a798f31
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-06-12 12:47:45 +0530

    [feat]: import AuhtProvider to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { AuthProvider } from "./Helper/Auth/auth-context";
 import { makeServer } from "./server";
 
 // Call make Server
@@ -10,9 +11,11 @@ makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
