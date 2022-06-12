8230e8f0501467cd4a10351fabcad8ba0b7fcd22
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-06-12 12:54:24 +0530

    [feat]: implement login functionality

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,7 +1,45 @@
 import "./login.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { login } from "../../Helper";
+import { useAuth } from "../../Helper/Auth/auth-context";
 
 const Login = () => {
+  const { dispatchUserState } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const previousRoute = location?.state?.from?.pathname || "/";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const emailHandler = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+  const testLoginHandler = (e) => {
+    e.preventDefault();
+    setEmail("chandler@friends.com");
+    setPassword("chandlerBing123");
+  };
+
+  const showPasswordHandler = (e) => {
+    e.preventDefault();
+    setShowPassword((prev) => !prev);
+  };
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    login(email, password, dispatchUserState, navigate, previousRoute);
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <main className="login flex_col flex_align__center mb_10 pt_8">
       <h1 className="h2 ">Login</h1>
@@ -12,8 +50,10 @@ const Login = () => {
         <input
           id="email_id"
           type="email"
+          value={email}
           placeholder="sheldon@tbbt.com"
           className="input block"
+          onChange={emailHandler}
           required
         />
         <div className="login_pwd">
@@ -21,13 +61,17 @@ const Login = () => {
             Password
           </label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             id="password"
             className="input block"
             placeholder="Your password here..."
+            value={password}
+            onChange={passwordHandler}
             required
           />
-          <button className="btn_icon btn_show__password">
+          <button
+            className="btn_icon btn_show__password"
+            onClick={showPasswordHandler}>
             <span className="material-icons outlined visibility ">
               visibility_off
             </span>
@@ -37,11 +81,15 @@ const Login = () => {
           <input type="checkbox" id="check_box" />
           <label htmlFor="check_box">Remember me</label>
         </div>
-        <button className="btn btn_primary btn_sm txt_semibold btn_login py_3 px_6">
+        <button
+          className="btn btn_primary btn_sm txt_semibold btn_login py_3 px_6"
+          onClick={loginHandler}>
           Login
         </button>
-        <button className="btn btn_sm mt_2 btn_test_login">
-          Login with test credentials
+        <button
+          className="btn btn_sm mt_2 btn_test_login"
+          onClick={testLoginHandler}>
+          Fill with test credentials
         </button>
         <a className="txt_sm forgot_pwd">Forgotten Password?</a>
         <hr className="hr my_3" />
